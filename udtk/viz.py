4e228db0d3f213c00120f54b836ab63db3510fdb
felipe <felipegonz@gmail.com>
2019-10-15 16:46:09 -0300

    viz module

---
+++ udtk/viz.py
@@ -0,0 +1,16 @@
+import contextily as ctx
+import matplotlib.pyplot as plt
+
+
+def add_basemap(ax, zoom, url='http://tile.stamen.com/terrain/tileZ/tileX/tileY.png'):
+    xmin, xmax, ymin, ymax = ax.axis('equal')
+    basemap, extent = ctx.bounds2img(xmin, ymin, xmax, ymax, zoom=zoom, url=url)
+    ax.imshow(basemap, extent=extent, interpolation='bilinear')
+    # restore original x/y limits
+    ax.axis((xmin, xmax, ymin, ymax))
+
+
+def plot_h3_gdf(h3_gdf, plot_column):
+    f, ax = plt.subplots(figsize=(8, 8))
+    h3_gdf.to_crs(epsg=3857).plot(ax=ax, column=plot_column, scheme='Quantiles')
+    add_basemap(ax, zoom=12, url=ctx.sources.ST_TONER_LITE)
