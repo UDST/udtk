d3174132a960fbd9ef6a82cc5ebd263cd7d4c2b8
felipe <felipegonz@gmail.com>
2019-10-15 15:19:27 -0300

    geo modulo notebook

--- udtk/geo.py
+++ udtk/geo.py
@@ -10,10 +10,12 @@ from shapely.geometry import Polygon
 def shapely_from_h3(h3_index):
     return Polygon([[i[1], i[0]] for i in h3.h3_to_geo_boundary(h3_index)])
 
-#we can split this function for loading csv or shape and turn into a table with x and y cols
-#the use a single function to turn this table into hexgrid
-def hexgrid_from_shapelfile(shapefile,res):
-    carto = gpd.read_file(shapefile)
+# we can split this function for loading csv or shape and turn into a table with x and y cols
+# the use a single function to turn this table into hexgrid
+
+
+def hexgrid_from_shapefile(input_shapefile, output_shapefile, res):
+    carto = gpd.read_file(input_shapefile)
 
     # make changes to get h3 grid
     carto = carto.to_crs(epsg=4326)
@@ -26,14 +28,16 @@ def hexgrid_from_shapelfile(shapefile,res):
     geoms = carto['h3_index'].map(shapely_from_h3)
     carto = gpd.GeoDataFrame(carto, geometry=geoms, crs={'init': 'epsg:4326', 'no_defs': True})
     carto = carto.to_crs({'init': 'epsg:3857', 'no_defs': True})
-    carto.to_file('carto/grid/grid.geojson',driver='GeoJSON')
+    carto.to_file(output_shapefile)
     return carto
 
-def weights_matrix(grid_id_name,shapefile = 'carto/grid/grid.geojson'):
+
+def weights_matrix(grid_id_name, input_shapefile='carto/grid/grid.shp', output_pickle='data/w.pickle'):
     # produce weights matrix
     w = ps.lib.weights.Queen.from_shapefile(
-        shapefile, idVariable = grid_id_name)
+        input_shapefile, idVariable=grid_id_name)
 
     # save data
-    with open('data/w.pickle', 'wb') as handle:
+    with open(output_pickle, 'wb') as handle:
         pickle.dump(w, handle)
+    return w
